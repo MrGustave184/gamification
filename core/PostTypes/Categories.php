79449db4772d058739c6a5cb496d601698f6e861
RafaelOrtiz <rafaeldavid184@gmail.com>
2020-11-05 08:48:44 -0400

    categories auto ACF

--- core/PostTypes/Categories.php
+++ core/PostTypes/Categories.php
@@ -7,6 +7,7 @@ class Categories
     public function register() 
     {
         add_action('init', [$this, 'categories_post_type']);
+        $this->add_ACF();
     }
 
     public function categories_post_type()
@@ -26,5 +27,76 @@ class Categories
             ],
             'menu_icon' => 'dashicons-games'
         ]);
+    }
+
+    private function add_ACF()
+    {
+        if( function_exists('acf_add_local_field_group') ):
+
+            acf_add_local_field_group(array(
+                'key' => 'group_5fa2f9305bc68',
+                'title' => 'gamification categories',
+                'fields' => array(
+                    array(
+                        'key' => 'field_5fa2f93bb17d6',
+                        'label' => 'category',
+                        'name' => 'category',
+                        'type' => 'text',
+                        'instructions' => '',
+                        'required' => 0,
+                        'conditional_logic' => 0,
+                        'wrapper' => array(
+                            'width' => '',
+                            'class' => '',
+                            'id' => '',
+                        ),
+                        'default_value' => '',
+                        'placeholder' => '',
+                        'prepend' => '',
+                        'append' => '',
+                        'maxlength' => '',
+                    ),
+                    array(
+                        'key' => 'field_5fa2f951b17d7',
+                        'label' => 'points',
+                        'name' => 'points',
+                        'type' => 'number',
+                        'instructions' => '',
+                        'required' => 0,
+                        'conditional_logic' => 0,
+                        'wrapper' => array(
+                            'width' => '',
+                            'class' => '',
+                            'id' => '',
+                        ),
+                        'default_value' => '',
+                        'placeholder' => '',
+                        'prepend' => '',
+                        'append' => '',
+                        'min' => '',
+                        'max' => '',
+                        'step' => '',
+                    ),
+                ),
+                'location' => array(
+                    array(
+                        array(
+                            'param' => 'post_type',
+                            'operator' => '==',
+                            'value' => 'gamif_categories',
+                        ),
+                    ),
+                ),
+                'menu_order' => 0,
+                'position' => 'normal',
+                'style' => 'default',
+                'label_placement' => 'top',
+                'instruction_placement' => 'label',
+                'hide_on_screen' => '',
+                'active' => true,
+                'description' => '',
+            ));
+            
+            endif;
     }
 }
